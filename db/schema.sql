bf12c3945c96d05179eff84b506dff9e840f7b97
Ismael Jimenez Jurado <ismael@eduphoria.net>
2023-08-14 15:18:04 -0600

    first changes to schema

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS CCG_DATABASE;
+CREATE DATABASE CCG_DATABASE;
+
+USE CCG_DATABASE;
+
+CREATE TABLE IF NOT EXISTS ccg_departments (
+  id int NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS ccg_roles (
+  id int NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title varchar(255) UNIQUE NOT NULL,
+  salary decimal UNSIGNED NOT NULL,
+  department_id int(255) NOT NULL,
+  INDEX depindex (department_id),
+    FOREIGN KEY (department_id) REFERENCES ccg_departments(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS ccg_employees (
+  id int NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  department varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ccg_ghouls (
+  id int NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  rating varchar(255) NOT NULL
+);
+
+
